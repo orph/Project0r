c3ff8095b3d4012eb966c8d31559475af13e385b
Alex Graveley <alex@beatniksoftware.com>
2010-05-31 07:11:12 -0700

    Some kind of baseline working.

--- views.py
+++ views.py
@@ -2,8 +2,11 @@
 import tornado.web
 from tornado import ioloop
 
+import multiprocessing
 import os
+import sys
 import time
+import urlparse
 
 class HomeHandler(tornado.web.RequestHandler):
     def get(self):
@@ -17,7 +20,28 @@ class PlayHandler(tornado.web.RequestHandler):
     mplayer_pid = None
     chrome_pid = None
 
-    def play(self, addr):
+    def kill_helpers(self):
+        if PlayHandler.wget_pid:
+            os.kill(PlayHandler.wget_pid, 9)
+            wget_pid = None
+        if PlayHandler.mplayer_pid:
+            os.kill(PlayHandler.mplayer_pid, 9)
+            wget_pid = None
+        if PlayHandler.chrome_pid:
+            os.kill(PlayHandler.chrome_pid, 9)
+            chrome_pid = None
+
+        try:
+            os.unlink("/tmp/vidfifo")
+        except OSError:
+            pass
+        try:
+            os.mkfifo("/tmp/vidfifo")
+        except OSError:
+            pass
+
+
+    def kill_xephyr(self):
         try:
             pid = open("/tmp/.X3-lock", 'r').read().strip()
             print 'killing pid:', pid
@@ -30,34 +54,28 @@ class PlayHandler(tornado.web.RequestHandler):
         except OSError:
             pass
 
+
+    def play(self, addr):
         addr = addr.replace('https://amnesia.mit.edu/oblivious/Movies/',
                             'https://marcos:pizzicato@amnesia.mit.edu/oblivious/Movies/')
+        addr = addr.replace('http://www.youtube.com/watch?',
+                            'http://www.youtube.com/watch_popup?')
 
-        #addr = addr.replace('http://www.youtube.com/watch?',
-        #                    'http://www.youtube.com/watch_videos?
+        addr_parse = urlparse.urlparse(addr)
 
-        if PlayHandler.wget_pid:
-            os.killpg(PlayHandler.wget_pid, 9)
-            wget_pid = None
-        if PlayHandler.chrome_pid:
-            os.kill(PlayHandler.chrome_pid, 9)
-            chrome_pid = None
+        self.kill_helpers()
 
-        if addr.endswith('.avi'):
+        if addr_parse.netloc != 'www.youtube.com':
             PlayHandler.wget_pid = os.fork()
             if PlayHandler.wget_pid == 0:
-                os.system('wget -q \'%s\' --no-check-certificate -O - | mplayer -fs -cache 8192 -' % addr)
+                os.execvp('wget', ['-q', addr, '--no-check-certificate', '-O', '/tmp/vidfifo'])
+            PlayHandler.mplayer_pid = os.fork()
+            if PlayHandler.mplayer_pid == 0:
+                os.execvp('mplayer', ['mplayer', '-idx', '-cache', '8192', '/tmp/vidfifo'])
         else:
             PlayHandler.chrome_pid = os.fork()
             if PlayHandler.chrome_pid == 0:
                 os.execvpe('google-chrome', ['google-chrome', '--app=%s' % addr], { 'DISPLAY': ':0' })
-        # run Xephyr
-        #if os.fork() == 0:
-        #    os.execvpe("Xephyr", ['Xephyr', ':3', '-ac', '-reset', '-terminate'], {})
-        #time.sleep(2)
-        # start google-chrome with addr fullscreen
-        #if os.fork() == 0:
-        #    os.execvpe('google-chrome', [addr], { 'DISPLAY': ':3' })
 
     def get(self):
         addr = self.get_argument('addr', None)
